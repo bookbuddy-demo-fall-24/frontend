3dee4c49d34ac3a34a65a14c02201e610333a546
sench-reviews <senchreviews@gmail.com>
2024-10-04 09:08:47 -0500

    Created basic nav

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navigations() {
-  return <div>Navigations</div>;
+  return (
+    <nav
+      style={{
+        width: "40%",
+        display: "flex",
+        justifyContent: "space-between",
+        margin: "0 auto",
+      }}
+    >
+      <Link to="/">See All Books</Link>
+      <Link to="/login">Login</Link>
+    </nav>
+  );
 }
 
 export default Navigations;
