5901a22be36cb7642ee816c62a339fa4cbaec537
sench-reviews <senchreviews@gmail.com>
2024-10-08 09:42:16 -0500

    Completed functionality for SingleBook component - fetches data for specified book and displays that data

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function SingleBook() {
-  return <div>SingleBook</div>;
+  const { bookid } = useParams();
+  const [book, setBook] = useState(null);
+
+  useEffect(() => {
+    axios(`${import.meta.env.VITE_API_BASE_URL}/api/books/${bookid}`)
+      .then((data) => {
+        console.log(data.data.book);
+        setBook(data.data.book);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h2>{book?.title}</h2>
+      <p>{book?.author}</p>
+      <img
+        src={book?.coverimage}
+        alt={`Cover image for ${book?.title} by ${book?.author}`}
+      />
+      <p>{book?.description}</p>
+    </div>
+  );
 }
 
 export default SingleBook;
