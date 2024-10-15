f890d066d001a57a5bbac8ab66ebabe42b1557ba
sench-reviews <senchreviews@gmail.com>
2024-10-15 08:52:40 -0500

    Added functionality to store token in localstorage, redirect user upon successful registration and to access token from localstorage upon refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import bookLogo from "./assets/books.png";
 import Books from "./components/Books";
@@ -12,6 +12,12 @@ import "../styles/main.scss";
 function App() {
   const [token, setToken] = useState(null);
 
+  useEffect(() => {
+    const localToken = localStorage.getItem("token");
+    if (localToken) {
+      setToken(localToken);
+    }
+  }, []);
   return (
     <>
       <h1>
--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Account() {
-  return <div>Account</div>;
+  return (
+    <div>
+      <h1>My Account</h1>
+    </div>
+  );
 }
 
 export default Account;
